74b528554b509f21b287c0d1c04ea64a5e2990ce
Justine Quiapos <jmzquiapos@outlook.com>
2025-01-06 18:44:15 +0000

    Updating CSS

--- wedding_invite/app/assets/js/app.js
+++ wedding_invite/app/assets/js/app.js
@@ -2,14 +2,21 @@ import "../css/app.css";
 import "./htmx.min.js";
 
 const cards = document.querySelectorAll(".flip-card-inner");
+const flipCardDesc = document.getElementsByClassName("flip-card-desc");
 
 [...cards].forEach((card) => {
   card.addEventListener("click", function () {
     card.style.transform += " rotateY(180deg) ";
     card.classList.toggle("is-flipped");
 
-    if card.classList.contains("is-flipped") {
-      
+    if (card.classList.contains("is-flipped")) {
+      console.log("card flipped");
+      flipCardDesc.hidden = false;
+      console.log(flipCardDesc.hidden);
+    } else {
+      console.log("card flipped");
+      flipCardDesc.hidden = true;
+      console.log(flipCardDesc.hidden);
     }
   });
 });
