2035ee0bbfb0108c5d2fb2ce234e71bec60a8e38
DaKii <jmzquiapos@outlook.com>
2025-01-06 22:41:10 +0000

    Adding env variables to settings

--- wedding_invite/config/app.rb
+++ wedding_invite/config/app.rb
@@ -4,7 +4,7 @@ require 'hanami'
 module WeddingInvite
   class App < Hanami::App
     config.actions.sessions = :cookie, {
-      key: 'bookshelf.session',
+      key: 'wedding_invite.session',
       secret: settings.session_secret,
       expire_after: 60 * 60 * 24 * 365
     }
--- wedding_invite/config/settings.rb
+++ wedding_invite/config/settings.rb
@@ -7,6 +7,8 @@ module WeddingInvite
     # setting :my_flag, default: false, constructor: Types::Params::Bool
 
     setting :database_url, constructor: Types::String
+    setting :db_username, constructor: Types::String
+    setting :db_password, constructor: Types::String
     setting :session_secret, constructor: Types::String
   end
 end
