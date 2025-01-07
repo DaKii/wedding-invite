5e6dd1a10e4138ca534a1abaeb8b583fbbdea276
Justine Quiapos <jmzquiapos@outlook.com>
2025-01-07 21:19:00 +0000

    Adding test location for debugging purposes

---
+++ src/app/actions/home/test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module WeddingInvite
+  module Actions
+    module Home
+      # Index class
+      class Test < WeddingInvite::Action
+        def handle(_request, response); end
+      end
+    end
+  end
+end
---
+++ src/app/views/home/test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module WeddingInvite
+  module Views
+    module Home
+      # View for Index
+      class Test < WeddingInvite::View
+        puts 'IN TEST'
+      end
+    end
+  end
+end
