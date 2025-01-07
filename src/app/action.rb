62333cbb4be15e814118344947ad7eb784de4d23
Justine Quiapos <jmzquiapos@outlook.com>
2025-01-07 21:27:33 +0000

    Adding basic error handling

--- src/app/action.rb
+++ src/app/action.rb
@@ -1,9 +1,18 @@
 # auto_register: false
 # frozen_string_literal: true
 
-require "hanami/action"
+require 'hanami/action'
 
 module WeddingInvite
   class Action < Hanami::Action
+    handle_exception StandardError => :error_handler
+
+    def error_handler(_req, res, exception)
+      res.status = 500
+      res.body = {
+        title: 'Server Error',
+        details: exception.message
+      }.to_json
+    end
   end
 end
